27a7fff29643e8515f58c1d783e7d19a4f447f74
William Benton <self@willbenton.com>
2019-03-06 15:21:55 -0600

    Updates to the binary confusion matrix
    The binary confusion matrix now shows normalized values in its legend
    and raw counts in tooltips

--- mlworkflows/plot.py
+++ mlworkflows/plot.py
@@ -1,5 +1,6 @@
 import altair as alt
 import pandas as pd
+import numpy as np
 
 MAX_POINTS = 2500
 
@@ -12,7 +13,7 @@ def plot_points(df, **kwargs):
     plot_df = df.sample(min(len(df), MAX_POINTS))
     return alt.Chart(plot_df).encode(**kwargs).mark_point().interactive()
     
-def binary_confusion_matrix(predictions, actuals, labels = None, normalize = False, width = 215, height = 215):
+def binary_confusion_matrix(predictions, actuals, labels = None, width = 215, height = 215):
     activate()
     from sklearn.metrics import confusion_matrix
     
@@ -22,24 +23,23 @@ def binary_confusion_matrix(predictions, actuals, labels = None, normalize = Fal
     
     assert(len(labels) == 2)
     
-    cm = confusion_matrix(predictions, actuals, labels=labels)
-    if normalize:
-        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    ccm = confusion_matrix(predictions, actuals, labels=labels)
+    ncm = ccm.astype('float') / ccm.sum(axis=1)[:, np.newaxis]
     
     def labelizer(labels):
         def labelize(tup):
             i, v = tup
-            return {'predicted' : labels[int(i / 2)], 'actual' : labels[i & 1], 'value' : v}
+            return {'predicted' : labels[int(i / 2)], 'actual' : labels[i & 1], 'raw_count' : v[0], 'value' : v[1]}
         return labelize
 
     labelize = labelizer(labels)
     
-    cmdf = pd.DataFrame([labelize(t) for t in enumerate(cm.ravel())])
+    cmdf = pd.DataFrame([labelize(t) for t in enumerate(zip(ccm.ravel(), ncm.ravel()))])
     c = alt.Chart(cmdf).mark_rect().encode(
         x='predicted:O',
         y='actual:O',
         color='value:Q',
-        tooltip=["value:Q"]
+        tooltip=["raw_count:Q"]
     ).properties(width=width, height=height)
 
     return (cmdf, c)
