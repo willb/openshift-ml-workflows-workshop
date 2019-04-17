e48d13a98eac422183559bcca9c31320e5bea088
sophwats <sowatson@redhat.com>
2019-04-17 15:02:46 -0400

    defined a class

--- mlworkflows/featurestfidf.py
+++ mlworkflows/featurestfidf.py
@@ -3,51 +3,56 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import HashingVectorizer
 
+class tfidfFeatureVector:
+    """Convert a collection of raw documents to tfidf feature vectors"""
 
-def hv_count(corpus, n_feats):
-    """
-    hashes words from the _corpus_ of text
-    to n_feats buckets.
-    """
+    def __init__(self):
+        self
 
-    hv = HashingVectorizer(norm=None, token_pattern='(?u)\\b[A-Za-z]\\w+\\b',
+    def hv_count(self, corpus, n_feats):
+        """
+        hashes words from the _corpus_ of text
+        to n_feats buckets.
+        """
+
+        hv = HashingVectorizer(norm=None, token_pattern='(?u)\\b[A-Za-z]\\w+\\b',
                            n_features=n_feats, alternate_sign=False)
-    hvcounts = hv.fit_transform(corpus)  # returns a sparse numpy matrix.
+        hvcounts = hv.fit_transform(corpus)  # returns a sparse numpy matrix.
 
-    return hvcounts
-
-
-def hv_tfidf(hv_counts, df):
-    """
-    transforms a _hv_counts_ counts matrix
-    into tfidf matrix with lables
-    """
-
-    tfidf_transformer = TfidfTransformer()
-    tf_idf = tfidf_transformer.fit_transform(hv_counts)
-    dense_tf_idf = tf_idf.toarray()
-    labled_vecs = pd.concat([df.reset_index()[["index", "label"]],
-                            pd.DataFrame(dense_tf_idf)], axis=1)
-    labled_vecs.columns = labled_vecs.columns.astype(str)
-
-    return labled_vecs
+        return hvcounts
 
 
-def features_tfidf(df):
-    """
-    computes tf-idf feature vectors for
-    data frame _df_
-    """
+    def hv_tfidf(self, hv_counts, df):
+        """
+        transforms a _hv_counts_ counts matrix
+        into tfidf matrix with lables
+        """
 
-    hash_counts = hv_count(df["text"], 8192)
-    features = hv_tfidf(hash_counts, df)
+        tfidf_transformer = TfidfTransformer()
+        tf_idf = tfidf_transformer.fit_transform(hv_counts)
+        dense_tf_idf = tf_idf.toarray()
+        labled_vecs = pd.concat([df.reset_index()[["index", "label"]],
+                                pd.DataFrame(dense_tf_idf)], axis=1)
+        labled_vecs.columns = labled_vecs.columns.astype(str)
 
-    return features
+        return labled_vecs
 
 
-def transform(df, **transform_params):
-    return features_tfidf(df)
+    def features_tfidf(self, df):
+        """
+        computes tf-idf feature vectors for
+        data frame _df_
+        """
+
+        hash_counts = self.hv_count(df["text"], 8192)
+        features = self.hv_tfidf(hash_counts, df)
+
+        return features
 
 
-def fit(self, **fit_params):
-    return self
+    def transform(self, df, **transform_params):
+        return self.features_tfidf(df)
+
+
+    def fit(self, **fit_params):
+        return self
