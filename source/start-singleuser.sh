b4bf7f5ccc81270dff337ec89aa4f687c6ece72c
William Benton <self@willbenton.com>
2019-08-09 07:57:00 -0500

    added start-singleuser.sh from Vašek's minimal-notebook image

---
+++ source/start-singleuser.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+
+set -x
+
+JUPYTER_PROGRAM_ARGS="$JUPYTER_PROGRAM_ARGS $NOTEBOOK_ARGS"
+
+if [ x"$JUPYTER_MASTER_FILES" != x"" ]; then
+    if [ x"$JUPYTER_WORKSPACE_NAME" != x"" ]; then
+        JUPYTER_WORKSPACE_PATH=/opt/app-root/src/$JUPYTER_WORKSPACE_NAME
+        setup-volume.sh $JUPYTER_MASTER_FILES $JUPYTER_WORKSPACE_PATH
+    fi
+fi
+
+JUPYTER_ENABLE_LAB=`echo "$JUPYTER_ENABLE_LAB" | tr '[A-Z]' '[a-z]'`
+
+if [[ "$JUPYTER_ENABLE_LAB" =~ ^(true|yes|y|1)$ ]]; then
+    JUPYTER_PROGRAM_ARGS="$JUPYTER_PROGRAM_ARGS --NotebookApp.default_url=/lab"
+else
+    if [ x"$JUPYTER_WORKSPACE_NAME" != x"" ]; then
+        JUPYTER_PROGRAM_ARGS="$JUPYTER_PROGRAM_ARGS --NotebookApp.default_url=/tree/$JUPYTER_WORKSPACE_NAME"
+    fi
+fi
+
+if [[ "$JUPYTER_PROGRAM_ARGS $@" != *"--ip="* ]]; then
+    JUPYTER_PROGRAM_ARGS="--ip=0.0.0.0 $JUPYTER_PROGRAM_ARGS"
+fi
+
+if [ -n "${JUPYTER_PRELOAD_REPOS}" ]; then
+    for repo in `echo ${JUPYTER_PRELOAD_REPOS} | tr ',' ' '`; do
+        echo "Checking if repository $repo exists locally"
+        REPO_DIR=$(basename ${repo})
+        if [ -d "${REPO_DIR}" ]; then
+            pushd ${REPO_DIR}
+            GIT_SSL_NO_VERIFY=true git pull --ff-only
+            popd
+        else
+            GIT_SSL_NO_VERIFY=true git clone ${repo} ${REPO_DIR}
+        fi
+    done
+fi
+
+set -eo pipefail
+
+if [[ "$NOTEBOOK_ARGS $@" != *"--ip="* ]]; then
+  NOTEBOOK_ARGS="--ip=0.0.0.0 $NOTEBOOK_ARGS"
+fi
+
+JUPYTER_PROGRAM_ARGS="$JUPYTER_PROGRAM_ARGS --config=/opt/app-root/etc/jupyter_notebook_config.py"
+
+if [ ! -z "$JUPYTER_ENABLE_LAB" ]; then
+    JUPYTER_PROGRAM="jupyter labhub"
+else
+    JUPYTER_PROGRAM="jupyterhub-singleuser"
+fi
+
+exec /opt/app-root/bin/start.sh $JUPYTER_PROGRAM $JUPYTER_PROGRAM_ARGS "$@"
